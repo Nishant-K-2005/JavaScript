9bd130936f16807d627dcee06c3ab6b18a165bb4
Nishant <nishantkarena10@gmail.com>
2024-09-30 11:14:34 +0530

    Non-Primitive DataTypes (Array|Objects|Functions)

---
+++ BasicsOfJavaScript/Non-primitive.js
@@ -0,0 +1,26 @@
+// ----------------ARRAY--------------------
+const arr = [1,2,3,4,5] // Array Of numbers
+const StrArr = ['ABC','xyz','sQr','404'] // Array of Strings
+
+
+// ----------------OBJECT--------------------
+const myObj = {
+    name: 'Nishant',
+    age: 19,
+    course: 'Btech CSE'
+}
+
+
+// -------------FUNCTIONS-----------------
+const func1 = function(x,y,z){
+    console.log('Addition = ',x+y+z)
+    console.log('Multiplication = ',x*y*z);
+}
+    // Arrow Functions
+const func2 = (arg)=>{
+    console.log("This is a ArrowFunction "+arg);
+}
+
+function func3(args){
+    console.log("This is a Normal Function "+args);
+}
