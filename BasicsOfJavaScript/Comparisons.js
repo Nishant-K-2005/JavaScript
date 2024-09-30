d6bfd6a2b2c156a6229681ee3f42915238551f0f
Nishant <nishantkarena10@gmail.com>
2024-09-30 10:47:32 +0530

    Comparisons b/w datatypes

---
+++ BasicsOfJavaScript/Comparisons.js
@@ -0,0 +1,10 @@
+console.log(null>0) // false
+console.log(null==0) // false
+console.log(null>=0) // true 
+/* EXPLANATION:- EqualityCheck(==) and Comparisons(<,>,<=,>=) work differently.
+                Comparisons Convert null to number treating it as 0.
+*/
+
+
+console.log("02"==2) // true
+console.log("2">1) //true
